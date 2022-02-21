ba2063d47652d9d029a0a2eef08285dab09995e5
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-02-21 13:09:12 +0800

    Add skeleton

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>8K</title>
+				<meta
+					name="viewport"
+					content="initial-scale=1.0, width=device-width"
+				/>
+				<meta property="og:title" content="8K" />
+				<meta property="description" content="Gadget Reviews" />
+				<meta property="og:description" content="Gadget Reviews" />
+				<meta
+					property="og:image"
+					content="https://firebasestorage.googleapis.com/v0/b/logicui-9667a.appspot.com/o/others%2Freview_banner.png?alt=media&token=13befc98-b3c6-40ba-8e1b-29a00fa127b0"
+				/>
+			</Head>
+		</>
+	);
+}
